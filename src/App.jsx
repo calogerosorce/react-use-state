80435e3ffe031bf5119594d8c487af26b0ee81e1
calogerosorce <calogerosorce00@hotmail.com>
2025-11-07 12:37:37 +0100

    Add metod click for selection title and active the paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Main from "./components/Main"
 
 function App() {
 
@@ -10,17 +10,7 @@ function App() {
           <h1>Learn Web development</h1>
         </div>
       </header>
-      <main>
-        <div className="container">
-          <div className="btn btn-primary m-2">
-            <h3>HTML</h3>
-          </div>
-          <div className="card p-3">
-            <h2>HTML</h2>
-            <p>HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</p>
-          </div>
-        </div>
-      </main>
+      <Main />
     </>
   )
 }
