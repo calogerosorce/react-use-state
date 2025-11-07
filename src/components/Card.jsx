3f62ae95f1239ffd1319e0e78028b9b04fb83c11
calogerosorce <calogerosorce00@hotmail.com>
2025-11-07 16:27:47 +0100

    Add logic for button and componets for card

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Card(props) {
+
+
+
+    return (
+        <div className="card p-3">
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+        </div>
+    )
+}
