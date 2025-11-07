7d16c92a9f968779838917a04e7941a74d11a418
calogerosorce <calogerosorce00@hotmail.com>
2025-11-07 12:04:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <div className="container">
+          <h1>Learn Web development</h1>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          <div className="btn btn-primary m-2">
+            <h3>HTML</h3>
+          </div>
+          <div className="card p-3">
+            <h2>HTML</h2>
+            <p>HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</p>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
